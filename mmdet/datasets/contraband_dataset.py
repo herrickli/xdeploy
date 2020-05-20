bbb1afc0b50b9521064501aa2d98d8e2be031d48
licheng <1091168853@qq.com>
2020-05-20 09:12:26 +0800

    commit

--- mmdet/datasets/contraband_dataset.py
+++
@@ -1,6 +0,0 @@
-from .coco import CocoDataset
-from .registry import DATASETS
-
-@DATASETS.register_module
-class contrabandDataset(CocoDataset):
-    CLASSES = ('gun', 'dagger', 'knife', 'bottle', 'scissors')
--- mmdet/datasets/voc_contraband_dataset.py
+++
@@ -1,6 +0,0 @@
-from .voc import VOCDataset
-from .registry import DATASETS
-
-@DATASETS.register_module
-class VOCcontrabandDataset(VOCDataset):
-    CLASSES = ('gun', 'dagger', 'knife', 'bottle', 'light_bottle', 'scissors')
